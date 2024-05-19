bf553e8d703fa6f5469700c6ffdab9d4b4721861
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-05-19 17:11:50 -0400

    Update authentification.py

--- app/authentification.py
+++ app/authentification.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+def login(username, password):
+    # Vous pouvez ajouter votre logique d'authentification ici
+    if username == "admin" and password == "password":
+        st.session_state['logged_in'] = True
+
+def check_login_status():
+    return st.session_state.get('logged_in', False)
 
