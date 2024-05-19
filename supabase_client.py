5f2e3ebb1582281a3f4aabf242ef4dcec95901fe
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-05-19 17:44:26 -0400

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,1 +1,10 @@
+from supabase import create_client, Client
+import config
+
+def init_supabase() -> Client:
+    supabase_url = config.SUPABASE_URL
+    supabase_key = config.SUPABASE_KEY
+    return create_client(supabase_url, supabase_key)
+
+supabase = init_supabase()
 
