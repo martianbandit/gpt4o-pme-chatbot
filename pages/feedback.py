7850c490422f578e6d58c2a0d1854c9a0b7bb827
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-05-20 22:58:47 -0400

    Update feedback.py

--- pages/feedback.py
+++ pages/feedback.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Feedback")
 st.write("Leave your feedback here.")
-st.input(placeholder="entrer vos commentaires ici...")
+st.text_input(placeholder="entrer vos commentaires ici...")
 st.slider("Évaluez notre chatbot:", 1, 5, 3)
 if st.button("Envoyer"):
     st.success("Votre feedback es très apprécié!")
