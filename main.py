a08baf1cee48656d6407996830c6edc6b4acf3b4
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-12-25 22:34:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from utils.auth import check_auth, login, logout
 st.set_page_config(page_title="Le Spécialiste du Vrac", page_icon="🛒", layout="wide")
 
 # Pages disponibles dans l'application
-pages = {
-    "Home": "pages/home.py",
-    "Chatbot": "pages/chatbot.py",
-    "Feedback": "pages/feedback.py",
-    "Metrics": "pages/metrics.py"
-}
+
 
 # Authentification de l'utilisateur
 if "username" not in st.session_state:
@@ -22,6 +17,13 @@ else:
     st.sidebar.title(f"Welcome, {st.session_state['username']}")
     selection = st.sidebar.radio("Go to", list(pages.keys()))
     st.experimental_rerun()
+    
+   pages = {
+    "Home": "pages/home.py",
+    "Chatbot": "pages/chatbot.py",
+    "Feedback": "pages/feedback.py",
+    "Metrics": "pages/metrics.py"
+}
 
     with open(pages[selection]) as f:
         exec(f.read())
